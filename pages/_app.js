4222f0efb483db361ba25d955f360f8ce818d65a
Sergey Sovgut <sergey.sovgut@outlook.com>
2021-02-03 16:25:43 +0200

    Added redirect to https protocol

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
-import '../styles/globals.css'
+import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }) {
+  if (
+    process.env.NODE_ENV === "production" &&
+    !window.location.href.includes("https")
+  ) {
+    window.location.href = window.location.href.replace("http", "https")
+  }
+
   return <Component {...pageProps} />
 }
 
